670a934d423bee352014420b6b241a8782373c1b
Igor Rodrigues Quintão <igorrodriguesquintao@gmail.com>
2025-11-26 15:37:11 -0300

    up DATA

---
+++ styles/PerimetroBDNPABEIRARIOassentamento_brasil_ba_2_style.js
@@ -0,0 +1,31 @@
+var size = 0;
+var placement = 'point';
+
+var style_PerimetroBDNPABEIRARIOassentamento_brasil_ba_2 = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    
+    var labelText = ""; 
+    var value = feature.get("");
+    var labelFont = "10px, sans-serif";
+    var labelFill = "#000000";
+    var bufferColor = "";
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var offsetX = 0;
+    var offsetY = 0;
+    var placement = 'point';
+    if ("" !== null) {
+        labelText = String("");
+    }
+    var style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(31,120,180,1.0)', lineDash: [11.552,5.776], lineCap: 'square', lineJoin: 'bevel', width: 2.888}),
+        text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, placement, bufferColor,
+                              bufferWidth)
+    })];
+
+    return style;
+};
